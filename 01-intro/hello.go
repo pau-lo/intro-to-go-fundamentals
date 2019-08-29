933b47e484907ccc724c719fc5a8dd1941043296
pau-lo <pau_lo_lop_ez@protonmail.com>
2019-08-29 11:39:26 -0700

    created our first go program with explanations

--- 01-intro/hello.go
+++ 01-intro/hello.go
@@ -1,8 +1,16 @@
-package main
+// skeleton of a basic hello go file
 
-import "fmt"
+// collection of files: executable file
+// generates a file that we can run
+package main // package declaration
 
-func main() {
+// standard library package format
+// linked to main ... A fmt is the raw formatter used by Printf etc.
+// Package fmt implements formatted I/O
+import "fmt" // import packages/libraries that we need
+
+// func tells go that we are about to create a function
+func main() { // main sets the name of the function () list of arguments to pass to the function
 
 	fmt.Println("Hi, there!")
 }
